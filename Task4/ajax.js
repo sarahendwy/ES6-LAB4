64bd6794963225292ca731f8ffada9e22fcfedb0
sarahendwy <111707392+sarahendwy@users.noreply.github.com>
2026-03-12 03:09:59 +0200

    Update Task4 changes

--- Task4/ajax.js
+++ Task4/ajax.js
@@ -5,40 +5,43 @@
 let xhr = new XMLHttpRequest()
 xhr.open("GET", "rockbands.json")
 
+let bands = document.getElementById("band")
+let artists = document.getElementById("artist")
+
+
 xhr.onreadystatechange = function(){
     if (xhr.readyState == 4)
-        if (xhr.status == 200 && xhr.status<300){
+        if (xhr.status >=200 && xhr.status<300){
             let data = JSON.parse(xhr.responseText)
+            console.log(data)
 
-            let bands = document.getElementById("band")
+            
             let keyBand = Object.keys(data)
-            for (let i =0; i < keyBand.length ; i++){
+            for (let i = 0; i < keyBand.length ; i++){
                 let optionBand = document.createElement("option")
                 optionBand.value = keyBand[i]
-                optionBand.innerHTML = keyBand[i].charAt(0).toUpperCase()+keyBand[i].slice(1)
+                optionBand.innerHTML = keyBand[i]
                 bands.appendChild(optionBand)
             }
 
             bands.onchange = function(){
-                let artists = document.getElementById("artist")
                 artists.innerHTML = '<option value = "">Plase select</option>'
                 let bandSelect = bands.value
 
-                if(bandSelect == "") return
+                if(bandSelect == "") return;
 
                 let art = data[bandSelect]
 
-                for(let j = 0; j <art.length; j++){
-                    let options = document.createElement("option")
-                    options.value = art[j].value
-                    options.innerHTML = art[j].name
-                    artists.appendChild(options)
+                for(let i = 0; i <art.length; i++){
+                    let optionArt = document.createElement("option")
+                    optionArt.value = art[i].value
+                    optionArt.innerHTML = art[i].name
+                    artists.appendChild(optionArt)
                 }
 
                 artists.onchange = function(){
-                    let link = artists.value
-                    if(link != ""){
-                        window.open(link)
+                    if(artists.value != ""){
+                        window.open(artists.value)
                     }
                 }
             }
